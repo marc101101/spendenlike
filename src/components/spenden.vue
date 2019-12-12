3d5580e9d0a323bec15552be66d61317344d2727
Markus Gude <progamer101101@gmx.de>
2019-12-12 18:23:57 +0100

    added spenden basic structure mobile

--- src/components/spenden.vue
+++ src/components/spenden.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-    <div style="background:red; height:30vh">X</div>
-    <div style=" height:30vh">X</div>
-    <div style="background:red; height:30vh">X</div>
+    <a href="/spenden">
+      <div class="stacked-blocks-mobile red">
+        <h1 class="small-text-padding">Für den Verein</h1>
+      </div>
+    </a>
+    <a href="/betterplace">
+    <div class="stacked-blocks-mobile background-current-project">
+      <h1>Für unser aktuelles Projekt</h1>
+       <img src="../assets/current-project.jpg">
+    </div>
+    </a>
+    <a href="/patreon">
+    <div class="stacked-blocks-mobile red">
+      <h1 class="small-text-padding">Patreon</h1>
+    </div>
+    </a>
   </div>
 </template>
 
@@ -19,4 +32,35 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.stacked-blocks-mobile{
+  height: calc((100vh - 66px) / 3);
+}
+
+h1{
+  padding: 3rem;
+}
+
+.small-text-padding{
+  padding: 5rem;
+  color: white;
+}
+
+img {
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 30vh;
+    margin-top: -33vh;
+    opacity: 0.3;
+}
+
+.background-current-project{
+  color: #212529;
+  background-position: center;
+}
+
+.red{
+  background: red;
+}
+
 </style>
