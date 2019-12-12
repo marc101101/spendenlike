2bbba6f2b02761cba32ed5af34800b7d543e79a5
Markus Gude <progamer101101@gmx.de>
2019-12-12 16:47:17 +0100

    added manifest

--- src/components/spenden.vue
+++ src/components/spenden.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>   
+  <div>
+    <div style="background:red; height:30vh">X</div>
+    <div style=" height:30vh">X</div>
+    <div style="background:red; height:30vh">X</div>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: "Spenden",
   data() {
