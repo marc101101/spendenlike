d2152d8c01113f895066b6a976e786c8a0c37e0a
Markus Gude <progamer101101@gmx.de>
2020-03-14 11:47:13 +0100

    added info panel *img missing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-prime-theme">
-       <a class="navbar-brand bg-prime-theme" href="/">
-      <img alt="logo" src="./assets/logo-white.png" width="50px" height="50px">
-     SPENDENLIKE</a>
+      <a class="navbar-brand bg-prime-theme" href="/">
+        <img alt="logo" src="./assets/logo-white.png" width="50px" height="50px" />
+        SPENDENLIKE
+      </a>
       <button
         class="navbar-toggler"
         type="button"
@@ -18,12 +19,10 @@
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-           <li v-for="item in items" class="nav-item active">
-            <a class="nav-link bg-prime-theme" v-bind:href="item.path">
-              {{ item.name }}
-            </a>
-          </li>        
-        </ul>        
+          <li v-for="item in items" class="nav-item active">
+            <a class="nav-link bg-prime-theme" v-bind:href="item.path">{{ item.name }}</a>
+          </li>
+        </ul>
       </div>
     </nav>
     <router-view />
@@ -51,21 +50,27 @@ export default {
 <style>
 /* latin-ext */
 @font-face {
-  font-family: 'Bebas Neue';
+  font-family: "Bebas Neue";
   font-style: normal;
   font-weight: 400;
   font-display: swap;
-  src: local('Bebas Neue Regular'), local('BebasNeue-Regular'), url(../static/fonts/JTUSjIg69CK48gW7PXoo9WdhyyTh89ZNpQ.woff2) format('woff2');
-  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+  src: local("Bebas Neue Regular"), local("BebasNeue-Regular"),
+    url(../static/fonts/JTUSjIg69CK48gW7PXoo9WdhyyTh89ZNpQ.woff2)
+      format("woff2");
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB,
+    U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
 }
 /* latin */
 @font-face {
-  font-family: 'Bebas Neue';
+  font-family: "Bebas Neue";
   font-style: normal;
   font-weight: 400;
   font-display: swap;
-  src: local('Bebas Neue Regular'), local('BebasNeue-Regular'), url(../static/fonts/JTUSjIg69CK48gW7PXoo9WlhyyTh89Y.woff2) format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+  src: local("Bebas Neue Regular"), local("BebasNeue-Regular"),
+    url(../static/fonts/JTUSjIg69CK48gW7PXoo9WlhyyTh89Y.woff2) format("woff2");
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+    U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
+    U+FEFF, U+FFFD;
 }
 
 #app {
@@ -77,18 +82,18 @@ export default {
 }
 
 .navbar-dark .navbar-toggler {
-    border-color: rgba(0,0,0,0);
+  border-color: rgba(0, 0, 0, 0);
 }
 
 .navbar-dark .navbar-brand {
-    font-size: 1.8rem;
+  font-size: 1.8rem;
 }
 
-.nav-link{
+.nav-link {
   font-size: 1.4rem;
 }
 
-.bg-prime-theme{
+.bg-prime-theme {
   background-color: #c62828 !important;
   color: white !important;
 }
--- src/components/info.vue
+++ src/components/info.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="stacked-blocks-mobile button red">
-    <h1 class="small-text-padding">SEPA</h1>
+  <div>
+    <div class="header-container">
+      <img src="../assets/info.png" height="100%" width="100%" />
+    </div>
+    <div class="bottom-container">
+      <a href="/über">
+        <div class="stacked-blocks-mobile button red">
+          <h1 class="small-text-white">Über uns</h1>
+        </div>
+      </a>
+      <a href="/betterplace">
+        <div class="stacked-blocks-mobile button white">
+          <h1 class="small-text-red">Aktuelles Projekt</h1>
+        </div>
+      </a>
+      <a href="/geschichte">
+        <div class="stacked-blocks-mobile button red">
+          <h1 class="small-text-white">Geschichte</h1>
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -18,7 +37,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .stacked-blocks-mobile {
-  height: 20vh;
+  height: 10vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -28,7 +47,27 @@ export default {
   background: #c62828;
 }
 
-.small-text-padding {
+.small-text-white {
   color: white;
+}
+
+.white {
+  background: white;
+}
+
+.small-text-red {
+  color: #c62828;
+}
+
+h1 {
+  margin-bottom: 0;
+}
+
+.bottom-container {
+  bottom: 0rem;
+}
+
+.header-container {
+  height: 60vh;
 }
 </style>
