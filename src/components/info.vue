7dbfd56d7fd072df8f813abae215f13f7b00ac86
Markus Gude <progamer101101@gmx.de>
2020-03-14 11:22:25 +0100

    added first button

--- src/components/info.vue
+++ src/components/info.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>   
+  <div class="stacked-blocks-mobile button red">
+    <h1 class="small-text-padding">SEPA</h1>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: "Info",
   data() {
@@ -18,4 +17,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.stacked-blocks-mobile {
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.red {
+  background: #c62828;
+}
+
+.small-text-padding {
+  color: white;
+}
 </style>
