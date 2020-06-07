ba29537b34d6d28fb071f36bfb8ffac5b7dda25e
Markus Gude <progamer101101@gmx.de>
2020-06-07 17:10:53 +0200

    vuex store (does not work)

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import "es6-promise/auto";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  }
+});
